5aaae06202a3cf32ae7cd45397f24d86a20a5c7c
SX <sheuxiang@gmail.com>
2022-01-09 20:45:29 +0800

    add screenshot capabilities

--- screens/ResultScreen.tsx
+++ screens/ResultScreen.tsx
@@ -1,8 +1,10 @@
-import React, { useMemo, useEffect, useRef } from 'react';
-import { Center, Box, Divider, ScrollView } from 'native-base';
-
-import { useAppDispatch, useAppSelector } from '../state/redux/hooks';
-
+import React, { useMemo, useRef } from 'react';
+import { View } from 'react-native';
+import { Button, Center, Box, Divider, ScrollView, Text } from 'native-base';
+import ViewShot from 'react-native-view-shot';
+import * as Sharing from 'expo-sharing';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { useAppSelector } from '../state/redux/hooks';
 import { computeResult } from '../state/StatisticsCalculator';
 import VelocityChart from '../components/result/VelocityChart';
 import StrokeCountChart from '../components/result/StrokeCountChart';
@@ -11,46 +13,93 @@ import Hidden from '../components/Hidden';
 import DPSChart from '../components/result/DPSChart';
 
 export default function ResultScreen({ navigation }) {
-  const dispatch = useAppDispatch();
   const annotationsInfo = useAppSelector(state => state.annotation);
   const { averageVelocities, strokeRates, strokeCounts, distancePerStroke } =
     useMemo(() => computeResult(annotationsInfo), [annotationsInfo]);
+  const viewShotRef = useRef<ViewShot | null>(null);
+
+  const openShareDialogAsync = async (uri: string) => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`Uh oh, sharing isn't available on your platform`);
+      return;
+    }
+
+    await Sharing.shareAsync(uri);
+  };
+
+  const takeScreenshot = async () => {
+    // const result = await captureRef(viewShotRef, {
+    //   snapshotContentContainer: true,
+    // }).catch(e => {
+    //   console.log(`Error: result screen ${e}`);
+    //   return null;
+    // });
+    // if (result === null) {
+    //   return;
+    // }
+    if (
+      viewShotRef.current !== null &&
+      viewShotRef.current.capture !== undefined
+    ) {
+      const uri = await viewShotRef.current.capture();
+      const originalOrientation =
+        await ScreenOrientation.getOrientationLockAsync();
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.PORTRAIT
+      );
+      openShareDialogAsync(uri)
+        .catch(e => console.log(`Error: result screen ${e}`))
+        .finally(async () => {
+          await ScreenOrientation.lockAsync(originalOrientation);
+        });
+    }
+  };
+
   return (
-    <ScrollView alwaysBounceVertical={true}>
-      <Center>
-        <Hidden isHidden={averageVelocities.length === 0}>
-          <>
-            <Box py={4}>
-              <VelocityChart velocities={averageVelocities} />
-            </Box>
-            <Divider thickness={4} bg="muted.300" />
-          </>
-        </Hidden>
-        <Hidden isHidden={strokeCounts.length === 0}>
-          <>
-            <Box py={4}>
-              <StrokeCountChart strokeCounts={strokeCounts} />
-            </Box>
-            <Divider thickness={4} bg="muted.300" />
-          </>
-        </Hidden>
-        <Hidden isHidden={strokeRates.length === 0}>
-          <>
-            <Box py={4}>
-              <StrokeRateChart strokeRates={strokeRates} />
-            </Box>
-            <Divider thickness={4} bg="muted.300" />
-          </>
-        </Hidden>
-        <Hidden isHidden={distancePerStroke.length === 0}>
-          <>
-            <Box py={4}>
-              <DPSChart dps={distancePerStroke} />
-            </Box>
-            <Divider thickness={4} bg="muted.300" />
-          </>
-        </Hidden>
-      </Center>
-    </ScrollView>
+    <>
+      <ScrollView alwaysBounceVertical={true}>
+        <ViewShot style={{backgroundColor: '#fff'}} ref={viewShotRef}>
+          <Center>
+            <Hidden isHidden={averageVelocities.length === 0}>
+              <>
+                <Box py={4}>
+                  <VelocityChart velocities={averageVelocities} />
+                </Box>
+                <Divider thickness={4} bg="muted.300" />
+              </>
+            </Hidden>
+            <Hidden isHidden={strokeCounts.length === 0}>
+              <>
+                <Box py={4}>
+                  <StrokeCountChart strokeCounts={strokeCounts} />
+                </Box>
+                <Divider thickness={4} bg="muted.300" />
+              </>
+            </Hidden>
+            <Hidden isHidden={strokeRates.length === 0}>
+              <>
+                <Box py={4}>
+                  <StrokeRateChart strokeRates={strokeRates} />
+                </Box>
+                <Divider thickness={4} bg="muted.300" />
+              </>
+            </Hidden>
+            <Hidden isHidden={distancePerStroke.length === 0}>
+              <>
+                <Box py={4}>
+                  <DPSChart dps={distancePerStroke} />
+                </Box>
+                <Divider thickness={4} bg="muted.300" />
+              </>
+            </Hidden>
+          </Center>
+        </ViewShot>
+      </ScrollView>
+      <Box>
+        <Button onPress={takeScreenshot}>
+          <Text>Screenshot</Text>
+        </Button>
+      </Box>
+    </>
   );
 }
