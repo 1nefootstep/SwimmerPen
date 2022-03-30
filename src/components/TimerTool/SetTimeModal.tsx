bac0cea9d89e14241f0b540ceeea0d7bbb1f92c0
SX <sheuxiang@gmail.com>
2022-03-30 11:04:56 +0800

    set time format

--- src/components/TimerTool/SetTimeModal.tsx
+++ src/components/TimerTool/SetTimeModal.tsx
@@ -8,6 +8,8 @@ import {
   InputRightAddon,
   Modal,
   WarningOutlineIcon,
+  Text,
+  Row,
 } from 'native-base';
 import { useAppDispatch } from '../../state/redux/hooks';
 import { editTimerStartTime } from '../../state/redux';
@@ -28,18 +30,51 @@ export default function SetTimeModal({
   positionMillis,
 }: SetTimeModalProps) {
   const dispatch = useAppDispatch();
-  const [currInput, setCurrInput] = useState('');
+  const [currInputMin, setCurrInputMin] = useState('');
+  const [currInputSec, setCurrInputSec] = useState('');
+  const [currInputMs, setCurrInputMs] = useState('');
   const [time, setTime] = useState<number | undefined>(undefined);
   const [isInvalid, setIsInvalid] = useState(false);
-  const handleChange = (text: string) => {
-    const num = Number(text);
-    if (isNaN(num)) {
+
+  const handleChangeMin = (text: string) => {
+    const numMin = Number(text);
+    const numSec = Number(currInputSec);
+    const numMs = Number(currInputMs);
+    if (isNaN(numMin) || isNaN(numSec) || isNaN(numMs)) {
       setIsInvalid(true);
     } else {
       setIsInvalid(false);
-      setTime(num * 1000);
+      setTime(numMin * 60000 + numSec * 1000 + numMs);
     }
-    setCurrInput(text);
+    setCurrInputMin(text);
+  };
+
+  // consider adding checks to make sure 0<= sec < 60
+  const handleChangeSec = (text: string) => {
+    const numMin = Number(currInputMin);
+    const numSec = Number(text);
+    const numMs = Number(currInputMs);
+    if (isNaN(numMin) || isNaN(numSec) || isNaN(numMs)) {
+      setIsInvalid(true);
+    } else {
+      setIsInvalid(false);
+      setTime(numMin * 60000 + numSec * 1000 + numMs);
+    }
+    setCurrInputSec(text);
+  };
+
+  // consider adding checks to make sure 0<= sec < 1000
+  const handleChangeMs = (text: string) => {
+    const numMin = Number(currInputMin);
+    const numSec = Number(currInputSec);
+    const numMs = Number(text);
+    if (isNaN(numMin) || isNaN(numSec) || isNaN(numMs)) {
+      setIsInvalid(true);
+    } else {
+      setIsInvalid(false);
+      setTime(numMin * 60000 + numSec * 1000 + numMs);
+    }
+    setCurrInputMs(text);
   };
 
   return (
@@ -51,13 +86,28 @@ export default function SetTimeModal({
           <Modal.Body>
             <FormControl isInvalid={isInvalid}>
               <InputGroup>
-                <Input
-                  keyboardType="decimal-pad"
-                  value={currInput}
-                  onChangeText={handleChange}
-                  w={120}
-                />
-                <InputRightAddon children={'s'} />
+                <Row alignItems="center" maxH={40}>
+                  <Input
+                    keyboardType="number-pad"
+                    value={currInputMin}
+                    onChangeText={handleChangeMin}
+                    w={45}
+                  />
+                  <Text> : </Text>
+                  <Input
+                    keyboardType="number-pad"
+                    value={currInputSec}
+                    onChangeText={handleChangeSec}
+                    w={45}
+                  />
+                  <Text> . </Text>
+                  <Input
+                    keyboardType="number-pad"
+                    value={currInputMs}
+                    onChangeText={handleChangeMs}
+                    w={45}
+                  />
+                </Row>
               </InputGroup>
               <FormControl.ErrorMessage
                 leftIcon={<WarningOutlineIcon size="xs" />}
